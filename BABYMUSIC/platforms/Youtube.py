aaddf8adfba023bdda5f1dbb406750d9b2024462
NOBITA PAPA <fifaworldincome@gmail.com>
2024-12-01 14:45:40 +0530

    Update Youtube.py

--- BABYMUSIC/platforms/Youtube.py
+++ BABYMUSIC/platforms/Youtube.py
@@ -10,8 +10,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from NOBITAMUSIC.utils.formatters import time_to_seconds
-from NOBITAMUSIC.utils.database import is_on_off
+from BABYMUSIC.utils.formatters import time_to_seconds
+from BABYMUSIC.utils.database import is_on_off
 
 
 def cookies():
